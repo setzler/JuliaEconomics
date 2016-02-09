fd3a38446030dea8edc4094e37795a7ea6875565
Bradley Setzler <bradley.setzler@gmail.com>
2016-02-08 20:22:26 -0600

    Fix numbered label

--- IntroStructuralEstimation/sim_consump_leisure.jl
+++ IntroStructuralEstimation/sim_consump_leisure.jl
@@ -5,7 +5,6 @@
 #############################################################################
 
 ####### Set Simulation Parameters #########
-cd("/Users/bradley/JuliaEconomics/IntroStructuralEstimation/")
 srand(123)           # set the seed to ensure reproducibility
 N = 1000             # set number of agents in economy
 gamma = .5           # set Cobb-Douglas relative preference for consumption
